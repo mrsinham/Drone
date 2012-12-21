16c14bba012455df41d804b98aaffc3586f984b7
Julien LEFEVRE <julien.lefevr@gmail.com>
2012-12-21 18:46:25 +0100

    adding more options

--- configuration/servers.js
+++ configuration/servers.js
@@ -2,6 +2,8 @@ var aServers = [
     {
         "name":"Twenga1",
         "hostname":"loully.ignorelist.com",
-        "url":"http://91.121.167.175/map/"
+        "url":"http://91.121.167.175/map/",
+        "watchdelay": 1, // delay in minute
+        "timeout": 2  // timeout in seconds
     }
 ];
