61e86dc527b0693a16834d7b2db330a06177d371
Julien LEFEVRE <julien.lefevr@gmail.com>
2012-12-22 11:12:02 +0100

    add sample response

---
+++ example/response.js
@@ -0,0 +1,29 @@
+{
+    "code": 200,
+    "response": "",
+    "applications": [{
+        "name": "ShowcaseItemRead",
+        "code": 200,
+        "response": "Items ok"
+    }, {
+        "name": "ShowcaseUserRead",
+        "code": 200,
+        "response": "User read"
+    }, {
+        "name": "ShowcaseRabbitMQ",
+        "code": 200,
+        "response": "Messaging system is ok"
+    }],
+    "environment": {
+        "server": "www-05",
+        "php": "5.3.10-1",
+        "twenga-web.so": "2012\/12\/17 11:46:32\n. 2e9b9fc Yann Grandmaitre Add TwengaCacheClientCLuster::UpdateFields\n",
+        "twengavoclient": false,
+        "twengacacheclient": false,
+        "memcache": "2.2.6",
+        "memcached": "2.1.0",
+        "amqp": "0.1",
+        "mongo": "1.2.6",
+        "mysqli": "0.1"
+    }
+}
